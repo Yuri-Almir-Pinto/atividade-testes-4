d8fcc0ea6fd7432486033f2dd1ec92414e8a890e
yuri-almir-pinto <yurialmirp@gmail.com>
2024-07-01 20:18:33 -0300

    Feito a tarefa.

--- src/api/countries_api.py
+++ src/api/countries_api.py
@@ -8,6 +8,7 @@
 # ########################################
 from flask import Flask, request, jsonify
 import requests # pip install requests
+
 app = Flask(__name__) 
 # Dictionary mapping ISO codes to country names
 country_data = {
@@ -27,15 +28,25 @@ def get_country_name():
         # teste do iso_code "ZZ" que não existe
         return jsonify({"error": "Country not found"}), 404 
 
+@app.route("/get_country_name_rest", methods=["GET"])
 def get_country_name_rest():
     """ 
-    TODO: implementar rota para fazer 
+    TODO:implementar rota para fazer 
     request para a api restcountries passando o código iso "BR"
     e retorna o nome do país
     - https://restcountries.com/v3.1/alpha/br
-    - retornar data['name']['common']
+    - retornar data[0]['name']['common']
     """
-    ...
+    iso_code = request.args.get("iso_code")
+    url = f"https://restcountries.com/v3.1/alpha/{iso_code.lower()}"
+    
+    response: requests.Response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return jsonify(data[0]['name']['common'])
+    else:
+        return jsonify({"error": "Country not found"}), 404
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/api/countries_test.py
+++ src/api/countries_test.py
@@ -1,7 +1,9 @@
 # ########################################
 # File: countries_test.py
 # ########################################
+from flask import Response
 import pytest
+import pdb
 from countries_api import app
 
 @pytest.fixture
@@ -38,6 +40,31 @@ def test_get_country_name_missing_iso_code(client):
     assert 'error' in data
     assert data['error'] == 'ISO code not provided'
 
-def test_api_rest_countries():
-    """ TODO: implementar teste da rota """
-    ...
+def test_get_country_name_rest_returns_ok_if_br(client):
+    response: Response = client.get("/get_country_name_rest?iso_code=BR")
+    assert response.status_code == 200
+    data = response.get_json()
+    assert data == "Brazil"
+    
+def test_get_country_name_rest_returns_ok_if_af(client):
+    response: Response = client.get("/get_country_name_rest?iso_code=AF")
+    assert response.status_code == 200
+    data = response.get_json()
+    assert data == "Afghanistan"
+
+def test_get_country_name_rest_returns_ok_if_gb(client):
+    response: Response = client.get("/get_country_name_rest?iso_code=gb")
+    assert response.status_code == 200
+    data = response.get_json()
+    assert data == "United Kingdom"
+
+def test_get_country_name_rest_returns_error_if_nonsense(client):
+    response: Response = client.get("/get_country_name_rest?iso_code=asiodbuawbd")
+    assert response.status_code == 404
+    data = response.get_json()
+    assert "error" in data
+    assert data["error"] == "Country not found"
+    
+def test_get_country_name_rest_returns_error_if_post(client):
+    response: Response = client.post("/get_country_name_rest?iso_code=asiodbuawbd")
+    assert response.status_code == 405
